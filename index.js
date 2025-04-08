b1707356c42da1ead4576b7273228472f7e41ccb
matt-kr <runnermatt07@hotmail.com>
2025-04-08 09:33:21 -0700

    Co-authored-by: taylorpatterson-sc <taylorpatterson-sc@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,16 @@
+// Create a variable that will hold your to-do list items.
+let todo = []; 
+function addToTodo(task){
+    todoList.push(task)
+    console.log(`Added '${task}' to the to-do list!`)
+}
+]
+
+
+// Add a function that takes in a string as input
+// and adds it to the to-do list.
+
+
+// Add an event handler to populate an HTML element
+// with your to-do list when the user clicks the appropriate button.
+
